1b59c92c244cd5ed192246426c68c55fe17faffe
maxwellermeav <maxwellermeav@gmail.com>
2026-01-27 10:53:43 -0600

    added string type checks and age-dependent responses.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.Design;
+using System.Linq;
 
 namespace InternConsoleApp
 {
@@ -8,23 +10,62 @@ namespace InternConsoleApp
         {
             //This program already works. Your task is to change what happens after the input,
             //and to ensure the correct datatypes are being used for the inputs.
+            string name = "";
+            while (true)
+            {
+                // Ask for user's name
+                Console.Write("Enter your name: ");
+                name = Console.ReadLine();
 
-            // Ask for user's name
-            Console.Write("Enter your name: ");
-            string name = Console.ReadLine();
+                //Checking for only letter inputs
+                if (!string.IsNullOrWhiteSpace(name) && name.All(char.IsLetter))
+                {
+                    break;
+                }
 
-            // Ask for user's age
-            Console.Write("Enter your age: ");
-            int age = int.Parse(Console.ReadLine());
+                Console.WriteLine("Invalid Input. Please use letters only (no numbers or symbols)");
 
-            // Generic output 
-            Console.WriteLine();
-            Console.WriteLine($"Hello {name}!");
-            Console.WriteLine($"You are {age} years old.");
+            }
+
+            //Setting age integer to then be updated by string parsing
+            int age = 0;
+            while (true)
+            {
+                // Ask for user's age
+                Console.Write("Enter your age: ");
+                string ageInput = Console.ReadLine();
+
+                //checking for only numeric inputs
+                if (int.TryParse(ageInput, out age) && age >= 0)
+                {
+                    break;
+                }
+                Console.WriteLine("Invalid Input. Please use Numbers Only (No letters or symbols");
+            }
+
+            //Boolean for Age-dependent Messgae
+            Console.WriteLine("\n");
+            if (age > 18)
+            {
+                Console.WriteLine($"Hello, {name},\nlet's go for a drink.");
+            }
+           
+            else if (age == 18)
+            {
+                Console.WriteLine($"Hello, {name},\nlet's have fun this year.");
+
+            }
+
+            else if (age < 18)
+            {
+                Console.WriteLine($"Hello, {name},\nlet's go to class.");
+            }
+
 
             Console.WriteLine();
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
+
         }
     }
 }
