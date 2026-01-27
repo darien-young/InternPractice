9e7f70f46f742e7f2fcaf0308d1830820e5faa8d
Darien <darien.nat.young@gmail.com>
2026-01-27 09:34:47 -0600

    Practice #1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Ms Young");
+﻿using System;
+
+namespace InternConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This program already works. Your task is to change what happens after the input,
+            //and to ensure the correct datatypes are being used for the inputs.
+
+            // Ask for user's name
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            // Ask for user's age
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            // Generic output 
+            Console.WriteLine();
+            Console.WriteLine($"Hello {name}!");
+            Console.WriteLine($"You are {age} years old.");
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
